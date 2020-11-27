ecc08969dc1b52caf5e851d0875e028e573163b9
srhtylmz <serhatyilmaz@Serhat-MacBook-Pro.local>
2020-11-27 22:41:35 +0200

    updates

---
+++ src/containers/welcome/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Logo from '../../assets/images/welcome-page-images/logo.png'
+
+const Index = () => {
+
+    return (
+        <img src={Logo} alt="ozgur" style={{witdh:100, height:100}}/>
+    )
+}
+
+export default Index
